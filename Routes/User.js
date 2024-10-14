08e4a1e367b917a7e664dc2ca7a3c2c4cdd03742
Mcheaven <115719615+FransManlangit@users.noreply.github.com>
2024-10-14 13:53:01 +0800

    FIX USER

--- Routes/User.js
+++ Routes/User.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const upload = require("../Utils/Multer");
-const { registerUser, verifyUserEmail, notificationAll, loginUser, logoutUser, getUserProfile } = require('../Controllers/UserController');
+const { registerUser, updateProfile, verifyUserEmail, notificationAll, loginUser, logoutUser, getUserProfile } = require('../Controllers/UserController');
 const { isAuthenticatedUser, authorizeRoles } = require("../Middlewares/Auth")
 
 
 
 router.route('/register').post(registerUser);
+router.put('/updateprofile', isAuthenticatedUser, upload.single("avatar"), updateProfile);
 router.route('/login').post(loginUser);
 router.route('/logout').get(logoutUser);
 router.get('/userprofile', isAuthenticatedUser, getUserProfile);
