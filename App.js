bb59168ac5de76039c7611d5df7333141d387f29
Mcheaven <115719615+FransManlangit@users.noreply.github.com>
2024-10-23 17:51:44 +0800

    fix app.js error

--- App.js
+++ App.js
@@ -2,34 +2,44 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
 const cors = require("cors");
-require('dotenv').config({path:'./config/.env'});
+
 
 
 const errorMiddleware = require("./middlewares/errors");
+require('dotenv').config({path:'./config/.env'});
+
 app.use(express.json({ limit: '100mb' }));
-app.use(express.urlencoded({ limit: "100mb", extended: true }));
-app.use(cookieParser());
-app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,
-}));
+
+
+// app.use(cors({
+//     origin: (origin, callback) => {
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     credentials: true,
+// }));
 
 const users = require('./Routes/User');
 const product = require('./Routes/Product');
 
+// app.use(
+//   cors({
+//     origin: `${process.env.FRONTEND_URL}`,
+//     credentials: true,
+//   })
+// );
+
 app.use(
   cors({
-    origin: `${process.env.FRONTEND_URL}`,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
-
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+app.use(cookieParser());
 
 
 app.use('/api/v1', users);
